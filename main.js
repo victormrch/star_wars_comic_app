224dd0beceb9763b630c0e24d93e0809b6d83572
“Victor <victor.raman@eu.jll.com  git config --global user.name Victor Ramongit config --global user.email victor.raman@eu.jll.com>
2021-04-15 20:56:26 +0200

    Created NavBarSection

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 const menuBtn = document.querySelector('.menu-btn');
+const navBar = document.querySelector('.navbar');
 let menuOpen = false;
 menuBtn.addEventListener('click', () => {
-  if(!menuOpen) {
+  if(!menuOpen) {  
+    navBar.style.display='';  
     menuBtn.classList.add('open');
     menuOpen = true;
   } else {
+    navBar.style.display='none';
     menuBtn.classList.remove('open');
     menuOpen = false;
   }
